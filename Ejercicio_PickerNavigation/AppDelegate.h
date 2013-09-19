83412847386592734195c6922a06ccbff9e976f8
Ma.Elena Melón Jareda <mmelon@MaElena-Melon-Jaredas-MacBook-Pro.local>
2013-09-19 16:29:50 -0500

    Initial Commit

---
+++ Ejercicio_PickerNavigation/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Ejercicio_PickerNavigation
+//
+//  Created by Ma.Elena Melón Jareda on 9/19/13.
+//  Copyright (c) 2013 Ma.Elena Melón Jareda App Studio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Ejercicio_PickerNavigation/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Ejercicio_PickerNavigation
+//
+//  Created by Ma.Elena Melón Jareda on 9/19/13.
+//  Copyright (c) 2013 Ma.Elena Melón Jareda App Studio. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
